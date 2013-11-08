cc5890f54267bc773addde23688d56ba98bb8c12
Jack Bracewell <jbtwentythree@gmail.com>
2013-11-08 19:33:12 +0000

    Only matching one colour at a time :(

--- plugin/helper.rb
+++ plugin/helper.rb
@@ -15,13 +15,51 @@ def run
   VIM::command("badd #{new_name}")
   new_buffer = VIM::Buffer[VIM::Buffer.count-1]
 
+  timestamps = []
   (1..size).each do |line|
     command = 'git blame ' + filename + ' -L ' + line.to_s + ',' + line.to_s + ' -t'
     git_out = VIM::evaluate("ShellCall('" + command + "')")
-    new_buffer.append line-1,  get_first_number(git_out)
+
+    timestamp = get_first_number git_out
+    new_buffer.append line-1, timestamp
+    timestamps << timestamp
   end
 
   VIM::command('vertical 20 new')
   VIM::command('edit ' + new_name)
   VIM::command('normal GGdd')
+
+  highlight_things timestamps
+end
+
+def highlight_things timestamps
+
+  different_colours = timestamps.uniq.size
+  start = 232
+  finish = 235
+  range = finish - start
+
+  sorted_stamps = timestamps.uniq.map(&:to_i).sort
+  smallest = sorted_stamps.first
+  biggest = sorted_stamps.last
+
+  colours = timestamps.map do |timestamp|
+    ((timestamp.to_i - smallest) / (biggest - smallest)).round + start
+  end
+
+  colours.uniq.each do |c|
+    VIM::command('highlight ' + 'col' + c.to_s + ' ctermbg=' + c.to_s + 'guibg=' + c.to_s)
+  end
+
+  colo_hash = {}
+  command = colours.each_with_index do |colour, index|
+    colo_hash['col' + colour.to_s] ||= []
+    colo_hash['col' + colour.to_s] << ('\%' + (index+1).to_s + 'l')
+  end
+
+  colo_hash.each do |hash, value|
+    reginald = '/' + value.join('\|') + '/'
+    VIM::command('match ' + hash + ' ' + reginald)
+  end
+
 end
