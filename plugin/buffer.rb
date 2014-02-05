0e04ed867c31963f31d84310fcbb4f8edcec7b9a
Jack Bracewell <jbtwentythree@gmail.com>
2014-02-05 02:22:04 +0000

    Buffer with detection of the 6 types of lines that could occur.

--- plugin/buffer.rb
+++ plugin/buffer.rb
@@ -1,14 +1,181 @@
+require File.join(File.dirname(__FILE__), 'group.rb')
+require File.join(File.dirname(__FILE__), 'sign.rb')
+
 class Buffer
 
-  attr_accessor :sequences, :sequence_scanner
+  attr_accessor :sequence_scanner
 
   def initialize filename
     @filename = filename
     @sequence_scanner = SequenceScanner.new(filename)
+    @_id = 0
+    @last_deleted_lines = []
+    @last_added_lines = []
+    @last_changed_lines = []
+  end
+
+  def groups
+    @_groups ||= {}
+  end
+
+  def signs
+    @_signs ||= {}
+  end
+
+  def get_new_id
+    @_id += 1
+    @_id
   end
 
   def find_current_sequence line
     @sequence_scanner.current_sequence line
+  end
+
+  def define_sign name, hlgroup
+    group = Group.new name, hlgroup
+    groups[name] = group
+  end
+
+  def place_sign line_no, hl, filename, line_content
+    id = get_new_id
+    VIM.command "sign place #{id} name=#{hl} line=#{line_no} file=#{filename}"
+    groups[hl].add_sign id
+    signs[id] = Sign.new line_no, hl, line_content
+  end
+
+  #Unplaces a "new" sign
+  def unplace_sign line_no
+    id, sign = signs.detect { |k,v| v.line == line_no && v.group == 'new' }
+    return nil if !sign
+
+    VIM::command "sign unplace #{id}"
+    groups[sign.group].remove_sign id
+    signs.delete id
+  end
+
+  #Move down all signs below the specified line, we are essentially
+  #inserting a line at this point
+  def move_signs_down original_lines
+    original_lines.each do |original_line|
+      line = original_line_to_line original_line
+      signs.each do |id, s|
+        if s.line > line
+          s.move_down
+        end
+      end
+    end
+  end
+
+  #Remove line
+  def move_signs_up original_lines
+    original_lines.each do |original_line|
+      line = original_line_to_line original_line
+      signs.each do |id, s|
+        if s.line > line
+          s.move_up
+        end
+      end
+    end
+  end
+
+  def original_line_to_line original_line
+    id, sign = signs.detect { |k,v| v.original_line == original_line }
+    sign ? sign.line : nil
+  end
+
+  def get_line_content line
+    return nil if line < 1
+    id, sign = signs.detect { |k,v| v.line == line }
+    sign ? sign.line_content : nil
+  end
+
+  def consider_last_change
+    added_lines = []
+    changed_lines = []
+    deleted_lines = []
+
+    get_diff.split(' ').each do |str|
+      break if '<>-'.include? str[0]
+
+      if str.include? 'a'
+        range = str.split('a')[1..-1]
+      elsif str.include? 'c'
+        range = str.split('c')[1..-1]
+      elsif str.include? 'd'
+        range = str.split('d')[0..-2]
+      end
+
+      range.each do |s|
+        r = extract_range(s)
+        if str.include? 'a'
+          r.each do |n| added_lines << n end
+        elsif str.include? 'c'
+          r.each do |n| changed_lines << n end
+        elsif str.include? 'd'
+          r.each do |n| deleted_lines << n end
+        end
+      end
+    end
+
+    lines_that_have_been_deleted = deleted_lines - @last_deleted_lines
+    lines_that_have_been_undeleted = @last_deleted_lines - deleted_lines
+
+    lines_that_have_changed = changed_lines - @last_changed_lines
+    lines_that_have_unchanged = @last_changed_lines - changed_lines
+
+    lines_that_have_been_added = added_lines - @last_added_lines
+    lines_that_have_been_unadded = @last_added_lines - added_lines
+
+    handle_deleted_lines lines_that_have_been_deleted
+    handle_added_lines lines_that_have_been_added
+    handle_changed_lines lines_that_have_changed
+    handle_undeleted_lines lines_that_have_been_undeleted
+    handle_unadded_lines lines_that_have_been_unadded
+    handle_unchanged_lines lines_that_have_unchanged
+
+    @last_added_lines = added_lines
+    @last_changed_lines = changed_lines
+    @last_deleted_lines = deleted_lines
+  end
+
+  def handle_deleted_lines lines
+    #TODO
+  end
+
+  def handle_added_lines lines
+    #TODO
+  end
+
+  def handle_changed_lines lines
+    #TODO
+  end
+
+  def handle_undeleted_lines lines
+    #TODO
+  end
+
+  def handle_unadded_lines lines
+    #TODO
+  end
+
+  def handle_unchanged_lines lines
+    #TODO
+  end
+
+  def extract_range str
+    if str.include? ','
+      Range.new *str.split(',').map(&:to_i)
+    else
+      Range.new *[str.to_i]*2
+    end
+  end
+
+  def get_diff
+    `diff #{@filename} #{temp_filename} | sed '/^[<|>|-]/ d' | tr '\n' ' '`
+  end
+
+  def temp_filename
+    '/tmp/' + @filename.sub('/', '') + 'funny'
   end
 
 end
--- spec/buffer_spec.rb
+++ spec/buffer_spec.rb
@@ -1,12 +1,304 @@
 require 'plugin/buffer.rb'
+require 'plugin/sequence_scanner.rb'
 require 'rspec'
 require 'ruby-debug'
 
 describe Buffer do
 
   before do
+    VIM = double
     filename = 'Necrophile'
+    sequence_scanner = double
+    SequenceScanner.should_receive(:new).with(filename) { sequence_scanner }
     @buffer = Buffer.new filename
   end
 
+  describe 'define sign' do
+    it 'Creates a new group' do
+      name = 'John'
+      hlname = 'Candy'
+      Group.should_receive(:new).with(name, hlname)
+      @buffer.define_sign name, hlname
+    end
+  end
+
+  describe 'place sign' do
+    before do
+      group = double
+      groups = { 'John' => group }
+      group.should_receive(:add_sign).with(1)
+      @buffer.should_receive(:groups) { groups }
+    end
+
+    it 'Places a sign from the right group' do
+      line_number = 1
+      hl = 'John'
+      filename = 'Necrophile'
+      command1 = "sign place 1 name=John line=1 file=Necrophile"
+      VIM.should_receive(:command).with command1
+      @buffer.place_sign line_number, hl, filename, ''
+    end
+
+    it 'Adds the sign to "Signs"' do
+      signs = {}
+      @buffer.should_receive(:signs) { signs }
+      VIM.should_receive(:command).with 'sign place 1 name=John line=1 file=Audiophile'
+      @buffer.place_sign 1, 'John', 'Audiophile', ''
+      signs.size.should == 1
+      sign = signs[1]
+      sign.line.should == 1
+      sign.original_line.should == 1
+      sign.group.should == 'John'
+    end
+  end
+
+  describe 'unplace sign' do
+    before do
+      group = double
+      groups = { 'new' => group }
+      group.should_receive(:remove_sign).with(1)
+      @buffer.should_receive(:groups) { groups }
+      sign = double
+      signs = { 1 => sign }
+      sign.should_receive(:line) { 1 }
+      sign.should_receive(:group).twice { 'new' }
+      @buffer.should_receive(:signs).twice { signs }
+    end
+
+    it 'Unplaces the right sign' do
+      VIM.should_receive(:command).with 'sign unplace 1'
+      @buffer.unplace_sign 1
+    end
+  end
+
+  describe 'find by original line' do
+    it 'finds by original line' do
+      sign = double
+      signs = { 1 => sign }
+      sign.should_receive(:original_line) { 1 }
+      sign.should_receive(:line) { 500 }
+      @buffer.should_receive(:signs) { signs }
+      @buffer.original_line_to_line(1).should == 500
+    end
+  end
+
+  describe 'get_line_content' do
+    before do
+      @sign1 = double
+      @sign1.stub(:line) { 1 }
+      @sign2 = double
+      @sign2.stub(:line) { 2 }
+      @sign3 = double
+      @sign3.stub(:line) { 3 }
+      @signs = { 1 => @sign1, 2 => @sign2, 3 => @sign3 }
+      @buffer.should_receive(:signs) { @signs }
+    end
+
+    it 'will find the right line if it exists, and return its content' do
+      @sign2.should_receive(:line_content) { '^___^' }
+      @buffer.get_line_content(2).should == '^___^'
+    end
+
+    it 'returns nil if the line is out of range' do
+      @buffer.get_line_content(0).should be_nil
+      @buffer.get_line_content(4).should be_nil
+    end
+  end
+
+
+  describe 'move signs down' do
+    pending
+  end
+
+  describe 'move signs up' do
+    pending
+  end
+
+  describe 'consider_last_changes' do
+    before do
+      VIM.stub(:evaluate)
+      VIM.stub(:command)
+      filename = 'Necrophile'
+      file_content = ['I am line 1',
+                      'I am line 2',
+                      '',
+                      '',
+                      'Something else',
+                      'I am someone else']
+
+      File.open('Necrophile', 'w') do |file|
+        file_content.each do |line|
+          file.puts line
+        end
+      end
+
+      @buffer.define_sign 'Old', 'Oldgroup'
+      @buffer.define_sign 'New', 'Newgroup'
+      @buffer.place_sign 1, 'Old', filename, 'I am line 1'
+      @buffer.place_sign 2, 'Old', filename, 'I am line 2'
+      @buffer.place_sign 3, 'Old', filename, ''
+      @buffer.place_sign 4, 'New', filename, ''
+      @buffer.place_sign 5, 'Old', filename, ''
+      @buffer.place_sign 6, 'Old', filename, 'Something else'
+      @buffer.place_sign 7, 'Old', filename, 'I am someone else'
+
+      @buffer.stub(:temp_filename) { 'temp-Necrophile' }
+    end
+
+    after do
+      `rm Necrophile`
+      `rm temp-Necrophile`
+    end
+
+    it 'get_diff works' do
+      new_file_content = ['I am line 1',
+                          'I am line 2',
+                          '',
+                          'Something else']
+
+      stub_temp_file new_file_content
+      @buffer.get_diff.strip.should == '4d3 6d4'
+    end
+
+    it 'Can detect which lines have been deleted' do
+      new_file_content = ['I am line 1',
+                          'I am line 2',
+                          '',
+                          'Something else']
+
+      stub_temp_file new_file_content
+      @buffer.should_receive(:handle_deleted_lines).with [4, 6]
+      @buffer.should_receive(:handle_added_lines).with []
+      @buffer.should_receive(:handle_changed_lines).with []
+      @buffer.should_receive(:handle_undeleted_lines).with []
+      @buffer.should_receive(:handle_unadded_lines).with []
+      @buffer.should_receive(:handle_unchanged_lines).with []
+      @buffer.consider_last_change
+    end
+
+    it 'Can detext which lines have been added' do
+      new_file_content = ['I am line 1',
+                          'I am line 2',
+                          '',
+                          '',
+                          '',
+                          'Something else',
+                          'I am someone else']
+
+      stub_temp_file new_file_content
+      @buffer.should_receive(:handle_deleted_lines).with []
+      @buffer.should_receive(:handle_added_lines).with [5]
+      @buffer.should_receive(:handle_changed_lines).with []
+      @buffer.should_receive(:handle_undeleted_lines).with []
+      @buffer.should_receive(:handle_unadded_lines).with []
+      @buffer.should_receive(:handle_unchanged_lines).with []
+      @buffer.consider_last_change
+    end
+
+    it 'Can detext which lines have been changed' do
+      new_file_content = ['I am line 1',
+                          'I am line 2',
+                          '',
+                          'Gonna make a change',
+                          'Gonna make a difference',
+                          'Gonna make it riiiiiiight']
+
+      stub_temp_file new_file_content
+      @buffer.should_receive(:handle_deleted_lines).with []
+      @buffer.should_receive(:handle_added_lines).with []
+      @buffer.should_receive(:handle_changed_lines).with [4,5,6]
+      @buffer.should_receive(:handle_undeleted_lines).with []
+      @buffer.should_receive(:handle_unadded_lines).with []
+      @buffer.should_receive(:handle_unchanged_lines).with []
+      @buffer.consider_last_change
+    end
+
+    it 'Can detect undeleted lines' do
+      new_file_content = ['I am line 1',
+                          'I am line 2',
+                          '',
+                          'Something else']
+      stub_temp_file new_file_content
+      @buffer.consider_last_change
+
+      next_new_file_content = ['I am line 1',
+                               'I am line 2',
+                               '',
+                               '',
+                               'Something else',
+                               'I am someone else']
+      stub_temp_file next_new_file_content
+      @buffer.should_receive(:handle_deleted_lines).with []
+      @buffer.should_receive(:handle_added_lines).with []
+      @buffer.should_receive(:handle_changed_lines).with []
+      @buffer.should_receive(:handle_undeleted_lines).with [4, 6]
+      @buffer.should_receive(:handle_unadded_lines).with []
+      @buffer.should_receive(:handle_unchanged_lines).with []
+      @buffer.consider_last_change
+    end
+
+    it 'Can detect unadded lines' do
+      new_file_content = ['I am line 1',
+                          'I am line 2',
+                          '',
+                          '',
+                          '',
+                          'Something else',
+                          'I am someone else']
+      stub_temp_file new_file_content
+      @buffer.consider_last_change
+
+      next_new_file_content = ['I am line 1',
+                               'I am line 2',
+                               '',
+                               '',
+                               'Something else',
+                               'I am someone else']
+      stub_temp_file next_new_file_content
+
+      @buffer.should_receive(:handle_deleted_lines).with []
+      @buffer.should_receive(:handle_added_lines).with []
+      @buffer.should_receive(:handle_changed_lines).with []
+      @buffer.should_receive(:handle_undeleted_lines).with []
+      @buffer.should_receive(:handle_unadded_lines).with [5]
+      @buffer.should_receive(:handle_unchanged_lines).with []
+      @buffer.consider_last_change
+    end
+
+    it 'Can detect unchanged lines' do
+      new_file_content = ['I am line 1',
+                          'I am line 2',
+                          '',
+                          'Gonna make a change',
+                          'Gonna make a difference',
+                          'Gonna make it riiiiiiight']
+      stub_temp_file new_file_content
+      @buffer.consider_last_change
+
+      next_new_file_content = ['I am line 1',
+                               'I am line 2',
+                               '',
+                               '',
+                               'Something else',
+                               'I am someone else']
+      stub_temp_file next_new_file_content
+
+      @buffer.should_receive(:handle_deleted_lines).with []
+      @buffer.should_receive(:handle_added_lines).with []
+      @buffer.should_receive(:handle_changed_lines).with []
+      @buffer.should_receive(:handle_undeleted_lines).with []
+      @buffer.should_receive(:handle_unadded_lines).with []
+      @buffer.should_receive(:handle_unchanged_lines).with [4,5,6]
+      @buffer.consider_last_change
+    end
+  end
+
+end
+
+def stub_temp_file arr
+  File.open('temp-Necrophile', 'w') do |file|
+    arr.each do |line|
+      file.puts line
+    end
+  end
 end
