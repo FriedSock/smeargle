e2462702fca6c72cdd644d923abee116afc9f234
Jack Bracewell <jbtwentythree@gmail.com>
2014-02-09 02:38:48 +0000

    DiffGatherer class to provide the line content when an a/d/c is being made, in the form of a neat hash.

---
+++ plugin/diff_gatherer.rb
@@ -0,0 +1,75 @@
+require 'generator'
+
+class DiffGatherer
+
+  def initialize file1, file2
+    @file1 = file1
+    @file2 = file2
+  end
+
+  def diff
+   raw = `diff #{@file1} #{@file2}`
+   out = ''
+   harvest_type = nil
+   line_range = nil
+   change_border = nil
+
+   {}.tap do |rethash|
+     rethash[:additions] = []
+     rethash[:deletions] = []
+     rethash[:changes] = []
+
+     raw.each_line do |line|
+       if match = line.match(/^([0-9]*(?:,[0-9]*)?)(a|d|c)([0-9]*(?:,[0-9]*)?)$/)
+
+         case match[2]
+         when 'a'
+           harvest_type = :add
+           line_range = rangify match[3]
+         when 'd'
+           harvest_type = :delete
+           line_range = rangify match[1]
+         when 'c'
+           harvest_type = :change
+           line_range = rangify match[3]
+
+           #For now, set deleted line content to nil, as its a pain to calculate
+           #and not needed
+           deletion_range = difference_rangify match[1], match[3]
+           deletion_range.each do |l|
+             rethash[:deletions] << { :line => l, :content => nil }
+           end
+
+         end
+       else
+         case harvest_type
+         when :add
+           if line_content = line.match(/^> (.*)$/)
+             rethash[:additions] << { :line => line_range.next, :content => line_content[1] }
+           end
+         when :delete
+           if line_content = line.match(/^< (.*)$/)
+             rethash[:deletions] << { :line => line_range.next, :content => line_content[1] }
+           end
+         when :change
+           if line_content = line.match(/^> (.*)$/)
+             rethash[:changes] << { :line => line_range.next, :content => line_content[1] }
+           end
+         end
+       end
+     end
+   end
+  end
+
+  def rangify string
+    Generator.new lambda { |s| s.first..s.last }.call string.split(',').map(&:to_i)
+  end
+
+  def difference_rangify *strings
+    s1, s2 = strings.map{|s| s.split(',').map(&:to_i) }
+    return [] if s1.reduce(:-) == s2.reduce(:-) || s1.size == 1 && s2.size == 1
+    start_difference =  s2.first - s1.first
+    (s2.last+1+start_difference)..s1.last
+  end
+
+end
---
+++ spec/diff_gatherer_spec.rb
@@ -0,0 +1,101 @@
+require 'plugin/diff_gatherer.rb'
+require 'rspec'
+require 'ruby-debug'
+
+describe DiffGatherer do
+
+  before do
+    file1 = 'file1'
+    file_content = ['I am line 1',
+                    'I am line 2',
+                    '',
+                    '',
+                    'Something else',
+                    'I am someone else']
+
+    File.open('file1', 'w') do |file|
+      file_content.each do |line|
+        file.puts line
+      end
+    end
+
+    @diff_gatherer = DiffGatherer.new 'file1', 'file2'
+  end
+
+
+  it 'works for additions' do
+    file_content = ['I am line 1',
+                    'I am line 2',
+                    '',
+                    '',
+                    '',
+                    '',
+                    'Something else',
+                    'Oh yeah',
+                    'I am someone else']
+
+    stub_file_content 'file2', file_content
+    @diff_gatherer.diff.should == {:additions => [{:line => 5, :content => '' },
+                                              {:line => 6, :content => '' },
+                                              {:line => 8, :content => 'Oh yeah'}],
+                                   :deletions => [],
+                                   :changes => []}
+  end
+
+  it 'works for deletions' do
+    file_content = ['I am line 1',
+                    '',
+                    '',
+                    'Something else',
+                    'I am someone else']
+
+    stub_file_content 'file2', file_content
+    @diff_gatherer.diff.should == {:additions => [],
+                                   :deletions => [{:line => 2, :content => 'I am line 2'}],
+                                   :changes => []}
+  end
+
+  it 'works for changes' do
+    file_content = ['I am lne 1',
+                    'I am line 2',
+                    '',
+                    '',
+                    'Smething else',
+                    'I am someone else']
+
+    stub_file_content 'file2', file_content
+    @diff_gatherer.diff.should == {:additions => [],
+                                   :deletions => [],
+                                   :changes => [{:line => 1, :content => 'I am lne 1'},
+                                                {:line => 5, :content => 'Smething else'}]}
+  end
+
+  it 'with a change compression' do
+    file_content = ['I am',
+                    '',
+                    '',
+                    'Smething else',
+                    'I am someone else',
+                    'The best arddoun']
+
+    stub_file_content 'file2', file_content
+    @diff_gatherer.diff.should == {:additions => [{:line => 6, :content => 'The best arddoun'}],
+                                   :deletions => [{:line => 2, :content => nil}],
+                                   :changes => [{:line => 1, :content => 'I am'},
+                                                {:line => 4, :content => 'Smething else'}]}
+  end
+
+after :all do
+    `rm file1`
+    `rm file2`
+  end
+
+end
+
+def stub_file_content filename, file_content
+    File.open(filename, 'w') do |file|
+      file_content.each do |line|
+        file.puts line
+      end
+    end
+end
