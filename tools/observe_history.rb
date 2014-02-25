ab77933c4275e9b6386c43d16822c6ff76c5cc45
Jack Bracewell <jbtwentythree@gmail.com>
2014-02-25 23:16:13 +0000

    observe_history is working using git diff output, aside from not actually checking out the required commit yet.

--- tools/observe_history.rb
+++ tools/observe_history.rb
@@ -1,16 +1,13 @@
-#require File.join(File.dirname(__FILE__), 'plugin/diff_gatherer.rb')
-#require 'ruby-debug'
-#
-TEMP_FILENAME = "temp1"
-TEMP_FILENAME_PATH = File.join(File.dirname(__FILE__), TEMP_FILENAME)
-TEMP_FILENAME2 = "temp2"
-TEMP_FILENAME_PATH2 = File.join(File.dirname(__FILE__), TEMP_FILENAME2)
+require 'ruby-debug'
+
+TEMP_FILENAME = "/tmp/temp1"
+TEMP_FILENAME2 = "/tmp/temp2"
 
 if __FILE__ == $0
   search_term, gunk = ARGV
   `cd #{File.dirname(__FILE__)}; ./search_history.sh '#{search_term}' > #{TEMP_FILENAME}`
 
-  raw = File.open(TEMP_FILENAME_PATH).read
+  raw = File.open(TEMP_FILENAME).read
   commits = raw.split(/^$/)
 
   commits.each do |c|
@@ -25,16 +22,15 @@ if __FILE__ == $0
     response = $stdin.gets.chomp
     exit if response == 'n'
 
-    `git show #{commit} | bundle exec ruby changed_lines.rb > #{TEMP_FILENAME}`
+    `git show #{commit} > #{TEMP_FILENAME}`
     #TODO: Checkout, might need to do some stashing and popping
 
-    raw = File.open(TEMP_FILENAME_PATH).read
-    file_changes = raw.split(/^$/)
-    file_changes.each do |change|
-      filename = change.match(/filename: (\S*)$/)[1]
-      File.open(TEMP_FILENAME_PATH2, 'w') { |f| f.write(change) }
-      PAT = File.join('tools', TEMP_FILENAME2)
-      `cd $(git rev-parse --show-toplevel); vim -O #{filename} #{PAT} < \`tty\` > \`tty\` `
+    raw = File.open(TEMP_FILENAME).read
+    filenames = raw.scan(/^diff --git a\/([\w\-.\/ ]+) b\/(?:[\w\-.\/ ]+)+$/)
+    file_changes = raw.split(/^diff --git a\/(?:[\w\-.\/ ]+) b\/(?:[\w\-.\/ ]+)+$/)[1..-1].zip filenames
+    file_changes.each do |change, filename|
+      File.open(TEMP_FILENAME2, 'w') { |f| f.write(change) }
+      `cd $(git rev-parse --show-toplevel); vim -O #{filename} #{TEMP_FILENAME2} < \`tty\` > \`tty\` `
     end
   end
 
