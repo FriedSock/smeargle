9cc2ef287d97939ba5f92e5e2e5a966ce485923e
Jack Bracewell <jbtwentythree@gmail.com>
2014-04-05 21:23:44 +0100

    Fork processes to calculate colourings (incase they take too long)

--- plugin/line_colourer.rb
+++ plugin/line_colourer.rb
@@ -4,12 +4,44 @@ class LineColourer
 
   # TODO, let users specify these in their .vimrc
   COLOUR_GROUPS = 6
+  NOT_FINISHED = "Not finished"
 
   def initialize filename
-    @timestamps = generate_timestamps filename
-    @linear_groups = generate_linear @timestamps
-    @clustered_groups = generate_cluster @timestamps
-    @author_groups = generate_authors filename
+    @filename = filename
+    timestamps = generate_timestamps @filename
+    fork { generate_cluster timestamps }
+    fork { generate_linear timestamps }
+    fork { generate_authors @filename }
+  end
+
+  def cluster_groups
+    harvest_groups_from_file 'cluster'
+  end
+
+  def author_groups
+    harvest_groups_from_file 'author'
+  end
+
+  def linear_groups
+    harvest_groups_from_file 'linear'
+  end
+
+  def harvest_groups_from_file type
+    file_name = "/tmp/.#{@filename}-#{type}"
+    if !File.exist? file_name
+      puts "#{type} colouring for is not ready yet"
+      return default_groups
+    end
+
+    file = File.open(file_name, "rb")
+    contents = file.read
+    file.close
+    if eval(contents) && contents == NOT_FINISHED
+      puts "#{type} colouring for is not ready yet"
+      default_groups
+    else
+      eval(contents)
+    end
   end
 
   def get_colour line_no
@@ -18,15 +50,8 @@ class LineColourer
   end
 
   def highlight_lines opts={}
-    default_opts = {
-      :reverse => true,
-      :type => :clustered
-    }
-
-    opts = default_opts.merge opts
-    highlight_file @timestamps, @filename, :reverse  => opts[:reverse], :type => opts[:type]
+    highlight_file opts
   end
-
 
   def git_blame_output filename
     LineColourer.git_blame_output filename
@@ -39,11 +64,16 @@ class LineColourer
   end
 
   def generate_authors filename
+    file_name = "/tmp/.#{filename}-author"
+    File.open(file_name, 'w+') { |f| f.write NOT_FINISHED }
+
     out = git_blame_output filename
     authors = out.scan(/^author (.*)$/)
     author_pairs = authors.uniq.map {|a| [a.first, authors.count(a)]}.sort {|x,y| y[1] <=> x[1]}
     named_authors = author_pairs[0..COLOUR_GROUPS-2].map {|p| p.first}
     colour_groups = authors.map { |a| named_authors.index a.first }.map {|e| e ? e : named_authors.length }
+
+    File.open(file_name, 'w+') { |f| f.write colour_groups }
   end
 
   def generate_timestamps filename
@@ -64,6 +94,9 @@ class LineColourer
   end
 
   def generate_linear timestamps
+    file_name = "/tmp/.#{@filename}-linear"
+    File.open(file_name, 'w+') { |f| f.write NOT_FINISHED }
+
     sorted_stamps = timestamps.map(&:to_i).uniq.sort
     smallest = sorted_stamps.first
     biggest = sorted_stamps.last
@@ -71,54 +104,59 @@ class LineColourer
 
     #Don't want to divide by 0 if the whole file is 1 timestamp
     if biggest == smallest
-      return timestamps.map { COLOUR_GROUPS  - 1 }
+      colours =  timestamps.map { COLOUR_GROUPS  - 1 }
+    else
+      colours = timestamps.map do |timestamp|
+        (((timestamp.to_i - smallest).to_f / (biggest - smallest))*range).round
+      end
     end
 
-    colours = timestamps.map do |timestamp|
-      (((timestamp.to_i - smallest).to_f / (biggest - smallest))*range).round
-    end
+    File.open(file_name, 'w+') { |f| f.write colours }
   end
 
   def generate_cluster timestamps
+    file_name = "/tmp/.#{@filename}-cluster"
+    File.open(file_name, 'w+') { |f| f.write NOT_FINISHED }
+
     cluster = Jenks.cluster timestamps.map(&:to_i), COLOUR_GROUPS
     colours = timestamps.map do |timestamp|
       cluster.find_index { |c| c.include? timestamp.to_i }
     end
+
+    File.open(file_name, 'w+') { |f| f.write colours }
   end
 
 
-  def highlight_file timestamps, filename, opts={}
-
+  def highlight_file opts={}
     default_opts = {
-      :reverse => false,
-      :type => :linear,
+      :reverse => true,
+      :type => :clustered,
       :start => 232
     }
     opts = default_opts.merge opts
 
-
-    unique_groups = @linear_groups.uniq.length
-    finish = opts[:start] + COLOUR_GROUPS - 1
-    @linear_line_colours = @linear_groups.map {|c| finish - c }
-
-    unique_groups = @clustered_groups.uniq.length
-    finish = opts[:start] + (unique_groups - 1)
-    @clustered_line_colours = @clustered_groups.map {|c| finish - c }
-
-    @author_line_colours = @author_groups.map { |c| opts[:start] + c }
-
     if opts[:type] == :linear
-      @line_colours = @linear_line_colours
-    elsif opts[:type] == :clustered
-      @line_colours = @clustered_line_colours
-    elsif opts[:type] = :author
-      @line_colours = @author_line_colours
-    end
+      finish = opts[:start] + (COLOUR_GROUPS - 1)
+      @line_colours = linear_groups.map {|c| finish - c }
 
+    elsif opts[:type] == :clustered
+      clustered_groups = cluster_groups
+      unique_groups = clustered_groups.uniq.length
+      finish = opts[:start] + (unique_groups - 1)
+      @line_colours = clustered_groups.map {|c| finish - c }
+
+    elsif opts[:type] = :author
+      @line_colours = author_groups.map { |c| opts[:start] + c }
+    end
 
     command = @line_colours.each_with_index do |colour, index|
       current_buffer.place_sign((index+1), "col#{colour}")
     end
+  end
+
+  def default_groups
+    number_of_lines = `wc -l #{@filename}`.split.first.to_i
+    ('0'*number_of_lines).split(//).map(&:to_i)
   end
 
 end
