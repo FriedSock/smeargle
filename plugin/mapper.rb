97f3000f5ee7e02c2a82e693bb85eaf4a8be5e4a
Jack Bracewell <jbtwentythree@gmail.com>
2014-05-05 17:27:13 +0100

    Mapper to go from original lines to new lines

---
+++ plugin/mapper.rb
@@ -0,0 +1,43 @@
+class Mapper
+
+  #take the format of [range, difference]
+  def initialize added_lines, deleted_lines
+    @map = []
+    merged_lines = (deleted_lines.map {|l| l.tap { |t| l[:type] = :del }} + added_lines.map {|l| l.tap { |t| l[:type] = :add }})
+    merged_lines = merged_lines.sort { |l1, l2| l1[:new_line] <=> l2[:new_line] }
+    first = true
+
+    running_diff = 0
+    region_start = 1
+    last = 1
+    last_type = nil
+    merged_lines.each do |l|
+      if l[:type] == :add
+        if last_type != :add || l[:original_line] > last + 1
+          @map << [[region_start,l[:new_line]], running_diff]
+        end
+        running_diff += 1
+        last_type = :add
+      else
+        @map << [[l[:original_line],l[:original_line]], nil]
+        if l[:original_line] > last + 1
+          @map << [[region_start,l[:new_line]], running_diff]
+        end
+        running_diff -= 1
+        last_type = :del
+      end
+      last = l[:original_line]
+      region_start = @map.last[0].last + 1
+      first = false
+    end
+    @map << [[(@map.last[0][1]+1), 10000], running_diff]
+  end
+
+  def map original_line
+    @map.each do |m|
+      range = m[0]
+      next if original_line > range.last
+      return m[1] ? original_line + m[1] : nil
+    end
+  end
+end
---
+++ spec/mapper_spec.rb
@@ -0,0 +1,81 @@
+require 'plugin/diff_gatherer.rb'
+require 'plugin/mapper.rb'
+require 'rspec'
+require 'ruby-debug'
+
+describe Mapper do
+  before do
+    file1 = 'file1'
+    file_content = ['I am line 1',
+                    'I am line 2',
+                    '',
+                    '',
+                    'Something else',
+                    'I am someone else']
+
+    File.open('file1', 'w') do |file|
+      file_content.each do |line|
+        file.puts line
+      end
+    end
+
+    @diff_gatherer = DiffGatherer.new 'file1', 'file2'
+  end
+
+  it 'works for one addition' do
+    file_content = ['I am line 1',
+                    'I am line 2',
+                    'nice line',
+                    '',
+                    '',
+                    'Something else',
+                    'I am someone else']
+
+    stub_file_content 'file2', file_content
+    mapper = Mapper.new @diff_gatherer.git_diff[:additions], @diff_gatherer.git_diff[:deletions]
+    mapper.map(1).should == 1
+    mapper.map(5).should == 6
+  end
+
+
+  it 'works' do
+    file_content = ['I am',
+                    '',
+                    '',
+                    'Smething else',
+                    'I am someone else',
+                    'The best arddoun']
+
+    stub_file_content 'file2', file_content
+    mapper = Mapper.new @diff_gatherer.git_diff[:additions], @diff_gatherer.git_diff[:deletions]
+    mapper.map(6).should == 5
+  end
+
+  it 'return nil when the line is gone' do
+    file_content = ['I am',
+                    'I am line 2',
+                    'nice line',
+                    'Smething else',
+                    'I am someone else',
+                    'The best arddoun']
+
+    stub_file_content 'file2', file_content
+    mapper = Mapper.new @diff_gatherer.git_diff[:additions], @diff_gatherer.git_diff[:deletions]
+    mapper.map(1).should == nil
+    mapper.map(6).should == 5
+  end
+
+  after :all do
+    `rm file1`
+    `rm file2`
+  end
+
+end
+
+def stub_file_content filename, file_content
+  File.open(filename, 'w') do |file|
+    file_content.each do |line|
+      file.puts line
+    end
+  end
+end
